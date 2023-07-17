0a96992c7058a80fa65ffdf0aebc1d2482a5a2b1
2023rahabu <uwamahorora@gmail.com>
2023-07-17 07:20:18 -0700

    program that prints all possible different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int a;
+	int n;
+				for (a = 0; a < 10; a++)
+				{
+				for (n = a + 1; n < 10; n++)
+				{
+				putchar('0' + a);
+				putchar('0' + n);
+					if (a != 8 || n != 9)
+					{
+					putchar(',');
+					putchar(' ');
+					}
+					}
+					}
+					return (0);
+					}
