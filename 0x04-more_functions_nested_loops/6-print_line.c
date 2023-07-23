79492b3f0280db1a411c520718f2c2c9c8ed06df
2023rahabu <uwamahorora@gmail.com>
2023-07-23 00:52:41 -0700

    straight line updated

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,12 +8,16 @@ void print_line(int n)
 {
 	int c;
 
-	c = 0;
-	while (c < n)
+	if (n <= 0)
 	{
-		_putchar('-');
-
-		c++;
+		_putchar('\n');
 	}
+	else
+	{
+		for (c = 0; c < n; c++)
+		{
+			_putchar('_');
+		}
 	_putchar('\n');
+	}
 }
