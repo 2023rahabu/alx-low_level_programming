b4e05f35e1a006d0034ead3872918529cc05cd66
2023rahabu <uwamahorora@gmail.com>
2023-07-24 12:48:52 -0700

    counting string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _strlen - string lenght
+* @s: string parameter
+* Return: 0success
+*/
+int _strlen(char *s)
+{
+int len = 0;
+while (*s != '\0')
+{
+len++;
+s++;
+}
+return (len);
+}
