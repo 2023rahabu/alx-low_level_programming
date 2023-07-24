05584319bfe2a0155fb8b6073abf7dea105dae78
2023rahabu <uwamahorora@gmail.com>
2023-07-24 08:50:58 -0700

    swaping two number

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - swap the value of two integers
+ * @a: parameter to be swaped
+ * @b: parameter to be swaped
+ * Return: 0 success
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
