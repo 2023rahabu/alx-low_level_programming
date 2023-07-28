3602124258acae2880bc5c6a46ee8f3bd5eedb43
2023rahabu <uwamahorora@gmail.com>
2023-07-28 08:21:32 -0700

    a function that reverse an integer

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,18 +8,23 @@
 */
 void reverse_array(int *a, int n)
 {
-	int i;
+	int i,j;
 
+	j = 0;
 	i = n - 1;
 
 	if (n <= 1 || a == NULL)
 	{
 		return;
 	}
-	while (i > 0)
+	while (i > j)
 	{
-		printf("%d, ", a[i]);
+		int temp;
+		temp = a[j];
+		a[j] = a[i];
+		a[i] = temp;
+
+		j++;
 		i--;
 	}
-	printf("%d\n", a[0]);
 }
