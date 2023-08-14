f7cec9cbd6a2cdc8c5fd58b0b89b6ff5afe5b677
2023rahabu <uwamahorora@gmail.com>
2023-08-14 11:23:12 -0700

    struct dog with different data type

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H_
+#define DOG_H_
+/**
+ * struct dog - structure name
+ * @name: parameter
+ * @age: parameter
+ * @owner: parameter
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
