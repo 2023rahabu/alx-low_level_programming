fa49fa1872b9c5e45f82ad709ef448dc6c6e1528
2023rahabu <uwamahorora@gmail.com>
2023-10-11 15:48:36 +0200

    function pointer to print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ *print_name - function that prints name
+ *@name: parameter name
+ *@f: call back function
+ *Return: name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
