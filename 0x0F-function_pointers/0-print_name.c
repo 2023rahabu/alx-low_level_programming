6595625390c244f902f513ade06957d7a1510e6d
2023rahabu <uwamahorora@gmail.com>
2023-10-11 15:59:54 +0200

    function pointer to print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-#include "function_pointers.h"
+#ainclude "function_pointers.h"
 /**
  *print_name - function that prints name
  *@name: parameter name
@@ -7,6 +7,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL || f != NULL)
+	if (name != NULL && f != NULL)
 		f(name);
 }
