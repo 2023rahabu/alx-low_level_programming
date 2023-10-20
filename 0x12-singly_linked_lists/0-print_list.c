e34187356eeb07d9461a20e1f073dc04957ea6bf
2023rahabu <uwamahorora@gmail.com>
2023-10-20 10:26:05 +0200

    singly linked list for string

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,13 +16,9 @@ size_t print_list(const list_t *h)
 	while (temp != NULL)
 	{
 		if (temp->str == NULL)
-		{
 			printf("[0] (nil)\n");
-		}
 		else
-		{
 			printf("[%u] %s\n", temp->len, temp->str);
-		}
 		temp = temp->next;
 		count++;
 	}
