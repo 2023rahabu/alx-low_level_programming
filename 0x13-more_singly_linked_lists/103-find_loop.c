127e0db252e7f6544836cefd6f5c417bcd797d7a
2023rahabu <uwamahorora@gmail.com>
2023-10-25 11:59:42 +0200

    task 13

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,34 +1,38 @@
 #include "lists.h"
 
 /**
- * find_listint_loop - finds the loop in a linked list
- * @head: head
+ * find_listint_loop - check if there is loop in a linked list and
+ *            counts the number of unique nodes in a looped linked list
+ * @head: A pointer to the head of the listint_t to check.
  *
- * Return: address of the node where the loop starts,
- * or NULL if there is no loop
+ * Return: 0 - if the list is not looped,
+ *         Otherwise - the address of where the loops starts.
  */
+
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *temp = head;
-	listint_t *currentNode = head;
+	listint_t *slow_p = head, *fast_p = head;
 
-	while (currentNode != NULL && currentNode->next != NULL)
+	if (head == NULL)
 	{
-		temp = temp->next;
-		currentNode = currentNode->next->next;
-		if (temp == currentNode)
+		return (NULL);
+	}
+
+	while (slow_p && fast_p && fast_p->next)
+	{
+		slow_p = slow_p->next;
+		fast_p = (fast_p->next)->next;
+
+		if (slow_p == fast_p)
 		{
-			break;
+			slow_p = head;
+			while (slow_p != fast_p)
+			{
+				slow_p = slow_p->next;
+				fast_p = fast_p->next;
+			}
+			return (fast_p);
 		}
 	}
-	if (currentNode == NULL || currentNode->next == NULL)
-		return (NULL);
-	temp = head;
-	while (temp != currentNode)
-	{
-		temp = temp->next;
-		currentNode = currentNode->next;
-	}
-
-	return (temp);
+	return (NULL);
 }
